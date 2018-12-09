39a7890cf872b65b0f996900a86720ef432dbefb
AhmedAdel333 <45667701+AhmedAdel333@users.noreply.github.com>
2018-12-09 16:14:29 +0200

    Add files via upload
    python script written in python 3.4 uses the dictionary attack technique to guess rar file password

---
+++ rarfile_cracker.py
@@ -0,0 +1,28 @@
+import rarfile
+import os
+
+
+def unrar(passwd, rar_file):
+	fileload = rarfile.RarFile(rar_file)
+	fileload.UNRAR_TOOL = "UnRAR.exe"
+	fileload.extractall("cracked_file", pwd = passwd)
+
+
+
+while True:
+	rar_file = input("enter rar file path: ")
+	pass_file = input("enter password list path: ")
+
+	passfile = open(pass_file, "r")
+	passwords = passfile.readlines()
+	for passwd in passwords:
+		passwd = passwd.rstrip("\n")
+		try:
+			unrar(passwd, rar_file)
+			print ("[+] PASSWORD FOUND & FILE CRACKED: " + passwd)
+			break;
+		except:
+			print ("[-] wrong password: " + passwd)
+
+	input("press any key to continue ...")
+	
